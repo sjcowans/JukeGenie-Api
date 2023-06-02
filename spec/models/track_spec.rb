47c8e6e20a57a53d5dea94b42c6039e5e1a30f38
Sean Cowans <76144522+sjcowans@users.noreply.github.com>
2023-06-02 09:37:57 -0600

    feat: Complete model setup

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Track, type: :model do
   describe 'relationships' do
-    it { should has_many :playlist_tracks }
+    it { should have_many :playlist_tracks }
   end
 end
