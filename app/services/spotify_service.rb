27b5d19be29d64ea933f6a7caf2158e2575ecbc7
Andrew Bowman <andrewtbowman@gmail.com>
2023-06-01 23:02:41 -0400

    feat: spotify service first draft recommendations and search requests

--- app/services/spotify_service.rb
+++ app/services/spotify_service.rb
@@ -1,13 +1,40 @@
 class SpotifyService
-  def self.get(url)
+  def initialize(access_token)
+    @access_token = access_token
+  end
+
+  def get(url)
     response = conn.get(url)
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.conn
-    Faraday.new(url: 'https://api.spotify.com/v1') do |request|
-      request.headers['Authorization'] = "Bearer #{ENV['SPOTIFY_TOKEN']}"
+  def conn
+    Faraday.new(url: 'https://api.spotify.com/v1/') do |conn|
+      conn.headers['Authorization'] = "Bearer #{@access_token}"
     end
+  end
+
+  def recommendations(seeds)
+    response = conn.get do |req|
+      req.url "recommendations"
+      req.params["seed_artists"] = seeds[:artists].join(",") if seeds[:artists]
+      req.params["seed_genres"] = seeds[:genres].join(",") if seeds[:genres]
+      req.params["seed_tracks"] = seeds[:tracks].join(",") if seeds[:tracks]
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def search(query, type, market, limit, offset)
+    response = conn.get do |req|
+      req.url 'search', {
+        query: query,
+        type: type,
+        market: market,
+        limit: limit,
+        offset: offset
+      }
+    end
+    JSON.parse(response.body, symbolize_names: true)
   end
 
   def self.genre_search
