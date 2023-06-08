0dc4388bc0aef393f7438e53c4b705835c95008a
Sean Cowans <76144522+sjcowans@users.noreply.github.com>
2023-06-08 15:32:22 -0600

    feat: Begin refresh button

--- app/serializers/playlist_serializer.rb
+++ app/serializers/playlist_serializer.rb
@@ -1,4 +1,4 @@
 class PlaylistSerializer
   include JSONAPI::Serializer
-  attributes :name, :longitude, :latitude, :input_address, :spotify_id, :range, :join_key
+  attributes :name, :longitude, :latitude, :input_address, :spotify_id, :range, :join_key, :host_id
 end
