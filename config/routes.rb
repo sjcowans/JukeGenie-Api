1fc09c2cdcdce46bb274dfaa01c0fdb7705dd123
Sean Cowans <76144522+sjcowans@users.noreply.github.com>
2023-07-19 15:42:25 -0600

    feat: Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
 
   namespace :api  do
     namespace :v1 do
